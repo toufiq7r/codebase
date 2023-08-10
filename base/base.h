611f5958fd308d04f621f687896a1d1c77e26b3a
Toufiq Shishir <toufiq_a24@outlook.com>
2023-08-10 08:24:08 -0700

    codebase - base -> added helper macros

--- base/base.h
+++ base/base.h
@@ -111,12 +111,57 @@
 // NOTE(Toufiq): Helper Macros
 #define Stmnt(S) do { S } while(0)
 
-#define AssertBreak() (*(int*)0 = 0)
+#if !defined(AssertBreak)
+#   define AssertBreak() (*(int*)0 = 0)
+#endif
 
 #if ENABLE_ASSERT
 #   define Assert(c) Stmnt( if (!(c)) { AssertBreak(); })
 #else
 #   define Assert(c)
 #endif
+
+#define Stringify_(S) #S
+#define Stringify(S) Stringify_(S)
+#define Glue_(A, B) A##B
+#define Glue(A, B) Glue_(A, B)
+
+#define ArrayCount(a) (sizeof(a)/sizeof(*(a)))
+
+#define IntFromPtr(p) (unsigned long long)((char*)p - (char*)0)
+#define PtrFromInt(n) (void*)((char*)0 + (n))
+
+#define Member(T, m) (((T*)0)->m)
+#define OffsetOfMember(T, m) IntFromPtr(&Member(T, m))
+
+#define Min(a, b) (((a) < (b)) ? (a) : (b))
+#define Max(a, b) (((a) > (b)) ? (a) : (b))
+#define Clamp(a, x, b) (((x) < (a)) ? (a) : \
+                        ((b) < (x)) ? (b) : (x))
+#define ClampTop(a, b) Min(a, b)
+#define ClampBot(a, b) Max(a, b)
+
+#define global static
+#define local static
+#define function static
+
+#define c_linkage_begin extern "C" {
+#define c_linkage_end }
+#define c_linkage extern "C"
+
+#include <string.h>
+#define MemoryZero(p, z) memset((p), 0, (z))
+#define MemoryZeroStruct(p) MemoryZero((p), sizeof(*(p)))
+#define MemoryZeroArray(a) MemoryZero((a), sizeof(a))
+#define MemoryZeroTyped(p, c) MemoryZero((p), sizeof(*(p))*(c))
+
+#define MemoryMatch(a, b, z) (memcmp((a), (b), (z)) == 0)
+
+#define MemoryCopy(d, s, z) memmove((d), (s), (z))
+#define MemoryCopyStruct(d, s) MemoryCopy((d), (s), \
+                                            Min(sizeof(*(d)), sizeof(*(s))))
+#define MemoryCopyArray(d, s) MemoryCopy((d), (s), Min(sizeof(s), sizeof(d)))
+#define MemoryCopyTyped(d, s, c) MemoryCopy((d), (s), \
+                                                Min(sizeof(*(d)), sizeof(*(s)))*(c))
 
 #endif // BASE_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,50 @@
 #include <stdio.h>
 #include <base.h>
 
+#define EvalPrint(x) printf("%s = %d\n", #x, (int)(x))
+
+struct TestStruct {
+    int a;
+    int b;
+    int c;
+    int d;
+};
+
 int main(int argc, const char* argv[]){
-    
+    int foo[100];
+    for (int i = 0; i < ArrayCount(foo); i += 1) {
+        foo[i] = i;
+    }
+
+    EvalPrint(ArrayCount(foo));
+
+    int bar[100];
+    MemoryCopyArray(bar, foo);
+    EvalPrint(bar[50]);
+    EvalPrint(MemoryMatch(foo, bar, sizeof(foo)));
+    MemoryZeroArray(bar);
+    EvalPrint(bar[50]);
+    EvalPrint(MemoryMatch(foo, bar, sizeof(foo)));
+
+    EvalPrint(OffsetOfMember(TestStruct, a));
+    EvalPrint(OffsetOfMember(TestStruct, b));
+    EvalPrint(OffsetOfMember(TestStruct, c));
+    EvalPrint(OffsetOfMember(TestStruct, d));
+
+    TestStruct t = {1, 2, 3, 4};
+    EvalPrint(t.a);
+    EvalPrint(t.d);
+    MemoryZeroStruct(&t);
+    EvalPrint(t.a);
+    EvalPrint(t.d);
+
+    EvalPrint(Min(1, 10));
+    EvalPrint(Min(10, 1));
+    EvalPrint(Max(1, 10));
+    EvalPrint(Max(10, 1));
+    EvalPrint(Clamp(1, 10, 100));
+    EvalPrint(Clamp(1, 0, 100));
+    EvalPrint(Clamp(1, 500, 100));
+
     return 0;
 }
