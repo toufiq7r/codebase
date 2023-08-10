7cc99dbcc6ac9256b861cb9c45d36d4871a56866
Toufiq Shishir <toufiq_a24@outlook.com>
2023-08-10 07:35:18 -0700

    codebase - base -> added helper macros

--- base/base.h
+++ base/base.h
@@ -2,7 +2,30 @@
 #define BASE_H
 
 // NOTE(Toufiq): Context Cracking
-#if defined(_MSC_VER)
+#if defined(__clang__)
+#   define COMPILER_CLANG 1
+#   if defined(_WIN32)
+#       define OS_WINDOWS 1
+#   elif defined(__gnu_linux__)
+#       define OS_LINUX 1
+#   elif defined(__APPLE__) && defined(__MACH__)
+#       define OS_MAC 1
+#   else
+#       error missing OS detection
+#   endif
+#   if defined(__amd64__) 
+#       define ARCH_X64 1
+#   elif defined(__i386__)      // TODO(Toufiq): verify this works on clang
+#       define ARCH_X86 1
+#   elif defined(__arm__)       // TODO(Toufiq): verify this works on clang
+#       define ARCH_ARM 1
+#   elif defined(__aarch64__)
+#       define ARCH_ARM64 1
+#   else
+#       error missing ARCH detection
+#   endif
+
+#elif defined(_MSC_VER)
 #   define COMPILER_CL 1
 #   if defined(_WIN32)
 #       define OS_WINDOWS 1
@@ -16,29 +39,6 @@
 #   elif defined(_M_ARM)
 #       define ARCH_ARM
 //TODO(Toufiq): ARM64?
-#   else
-#       error missing ARCH detection
-#   endif
-
-#elif defined(__clang__)
-#   define COMPILER_CLANG 1
-#   if defined(_WIN32)
-#       define OS_WINDOWS 1
-#   elif defined(__gnu_linux__)
-#       define OS_LINUX 1
-#   elif defined(__APPLE__) && defined(__MACH__)
-#       define OS_MAC 1
-#   else
-#       error missing OS detection
-#   endif
-#   if defined(__amd64__)
-#       define ARCH_X64 1
-#   elif defined(__i386__) // TODO(Toufiq): verify this works on clang
-#       define ARCH_X86 1
-#   elif defined(__arm__)
-#       define ARCH_ARM 1
-#   elif defined(__aarch64__)
-#       define ARCH_ARM64 1
 #   else
 #       error missing ARCH detection
 #   endif
@@ -102,6 +102,21 @@
 #endif
 #if !defined(ARCH_ARM64)
 #   define ARCH_ARM64 0
+#endif
+
+#if !defined(ENABLE_ASSERT)
+#   define ENABLE_ASSERT
+#endif
+
+// NOTE(Toufiq): Helper Macros
+#define Stmnt(S) do { S } while(0)
+
+#define AssertBreak() (*(int*)0 = 0)
+
+#if ENABLE_ASSERT
+#   define Assert(c) Stmnt( if (!(c)) { AssertBreak(); })
+#else
+#   define Assert(c)
 #endif
 
 #endif // BASE_H
